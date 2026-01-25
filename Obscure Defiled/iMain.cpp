6d617b37300e2ec91f734a7c7bb317689828c3ba
Mohammed Shaheer Imam <imshaheer2004@gmail.com>
2026-01-26 03:03:54 +0600

    bg song added

--- Obscure Defiled/iMain.cpp
+++ Obscure Defiled/iMain.cpp
@@ -15,26 +15,27 @@ using namespace std;
 vector<int> menu_images;
 stack<string> screens;
 MenuScreen menu;
+int bgm_audio = -1;
 /* -------------------- DRAW -------------------- */
 void iDraw()
 {
-    iClear();
-    iSetColor(255, 255, 255);
-    
-    if (screens.top() == "Menu")
-    {
-        menu.drawMenuScreen(menu_images);
-    }
-    else if (screens.top() == "Game")
-    {
-        // Draw game screen
+	iClear();
+	iSetColor(255, 255, 255);
+
+	if (screens.top() == "Menu")
+	{
+		menu.drawMenuScreen(menu_images);
+	}
+	else if (screens.top() == "Game")
+	{
+		// Draw game screen
 		cout << "In Game Screen" << endl;
-    }
-    else if (screens.top() == "Settings")
-    {
-        // Draw settings screen
+	}
+	else if (screens.top() == "Settings")
+	{
+		// Draw settings screen
 		cout << "In Settings Screen" << endl;
-    }
+	}
 }
 
 /* -------------------- INPUT -------------------- */
@@ -42,47 +43,52 @@ void iMouseMove(int mx, int my) {}
 void iPassiveMouseMove(int mx, int my) {}
 void iMouse(int button, int state, int mx, int my)
 {
-    if (state == GLUT_DOWN && screens.top() == "Menu")
-    {
-        // Handle menu selection based on mouse position
-        if (menu.isPlayButtonClicked(mx, my))
-        {
-            screens.push("Game");
-        }
-        else if (menu.isSettingsButtonClicked(mx, my))
-        {
-            screens.push("Settings");
-        }
-        else if (menu.isQuitButtonClicked(mx, my))
-        {
-            exit(0);
-        }
-    }
+	if (state == GLUT_DOWN && screens.top() == "Menu")
+	{
+		// Handle menu selection based on mouse position
+		if (menu.isPlayButtonClicked(mx, my))
+		{
+			iStopSound(bgm_audio); 
+			screens.push("Game");
+		}
+		else if (menu.isSettingsButtonClicked(mx, my))
+		{
+			screens.push("Settings");
+		}
+		else if (menu.isQuitButtonClicked(mx, my))
+		{
+			iStopSound(bgm_audio); 
+			exit(0);
+		}
+	}
 }
 void iKeyboard(unsigned char key)
 {
-    if (key == 27) // ESC key
-    {
-        if (screens.size() > 1)
-        {
-            screens.pop(); // Go back to previous screen
-        }
-    }
+	if (key == 27) // ESC key
+	{
+		if (screens.size() > 1)
+		{
+			screens.pop();
+			if (screens.top() == "Menu")
+			{
+				bgm_audio = iPlaySound("resources//menu_screen//bg_audio.wav", 1.0, true); // Restart menu music
+			}
+		}
+	}
 }
 
 void iSpecialKeyboard(unsigned char key)
 {
-    if (key == GLUT_KEY_RIGHT)
-    {
-        cout << screens.top() << endl;
-		printf("%d",screens.top());
-        //bgX -= SCROLL_SPEED;  // move background left
-		//charX +=10;
-		//idx+=1;
-		//if(idx==8)
-			//idx = 0;
-
-    }
+	if (key == GLUT_KEY_RIGHT)
+	{
+		cout << screens.top() << endl;
+		printf("%d", screens.top());
+		// bgX -= SCROLL_SPEED;  // move background left
+		// charX +=10;
+		// idx+=1;
+		// if(idx==8)
+		// idx = 0;
+	}
 }
 
 /* -------------------- INIT -------------------- */
@@ -95,24 +101,24 @@ void iSpecialKeyboard(unsigned char key)
 // 		char a[100];
 // 		sprintf_s(a,"Images//c%d.png",i+1);
 // 		printf("%s",a);
-// 		charImg[i]=iLoadImage(a); 
+// 		charImg[i]=iLoadImage(a);
 // 	}
 // }
 
 void moveBG()
 {
-	//bgX -= 1;
+	// bgX -= 1;
 }
 /* -------------------- MAIN -------------------- */
 int main()
-{	
-    //iSetTimer(50,moveBG);
+{
+	// iSetTimer(50,moveBG);
 	iInitialize(SCREEN_WIDTH, SCREEN_HEIGHT, "Connected Background Scroll");
-	
-	menu_images=menu.initmenubar();
-	screens.push("Menu");
-	//menu_images[1] = menu.initmenubar1();
 
-    iStart();
-    return 0;
+	menu_images = menu.initmenubar();
+	screens.push("Menu");
+	// menu_images[1] = menu.initmenubar1();
+	bgm_audio = iPlaySound("resources//menu_screen//bg_audio.wav", 1.0, true);
+	iStart();
+	return 0;
 }
