2dc3995d1ab30527ae14ce56fab026facb93edf7
shaheer-aust <shaheer.cse.00724205101034@aust.edu>
2026-01-25 14:31:35 +0600

    ok

--- Obscure Defiled/Screens/menu_screen.hpp
+++ Obscure Defiled/Screens/menu_screen.hpp
@@ -1,18 +1,6 @@
 #ifndef MENU_SCREEN_H
 #define MENU_SCREEN_H
-#include "iGraphics.h"
-#include<bits/stdc++.h>
 #include<iostream>
 using namespace std;
 
-struct menu_screen
-{
-    void draw()
-    {
-        int background = iLoadImage("resources//menu_screen//menu.jpg");
-        iShowImage(0, 0, 1280, 720, background);
-       
-    }
-    
-};
 #endif
