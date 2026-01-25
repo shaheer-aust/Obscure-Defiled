273495c1270354168d0d0d1040829584aa3a1a9e
Mohammed Shaheer Imam <imshaheer2004@gmail.com>
2026-01-25 14:28:37 +0600

    menu test

--- Obscure Defiled/iMain.cpp
+++ Obscure Defiled/iMain.cpp
@@ -8,6 +8,7 @@
 
 /* -------------------- GLOBALS -------------------- */
 int bg1, bg2;
+int bg;
 int bgX = 0;   // scrolling offset
 int charX = 0;
 int idx = 0;
@@ -19,15 +20,15 @@ void iDraw()
     iSetColor(255, 255, 255);
 
     // Draw connected backgrounds
-    iShowImage(bgX, 0, SCREEN_WIDTH, SCREEN_HEIGHT, bg1);
-    iShowImage(bgX + SCREEN_WIDTH, 0, SCREEN_WIDTH, SCREEN_HEIGHT, bg2);
-	iShowImage(charX, 100, 50, 100, charImg[idx]);
+    iShowImage(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, bg);
+    // iShowImage(bgX + SCREEN_WIDTH, 0, SCREEN_WIDTH, SCREEN_HEIGHT, bg2);
+	// iShowImage(charX, 100, 50, 100, charImg[idx]);
 
     // Wrap background for continuous effect
-    if (bgX <= -SCREEN_WIDTH)
-        bgX = 0;
-	if(charX>=SCREEN_WIDTH-10)
-		charX=0;
+    // if (bgX <= -SCREEN_WIDTH)
+    //     bgX = 0;
+	// if(charX>=SCREEN_WIDTH-10)
+	// 	charX=0;
 }
 
 /* -------------------- INPUT -------------------- */
@@ -50,17 +51,22 @@ void iSpecialKeyboard(unsigned char key)
 }
 
 /* -------------------- INIT -------------------- */
-void initBackground()
+// void initBackground()
+// {
+//     bg1 = iLoadImage("Images//bg1.jpg");
+//     bg2 = iLoadImage("Images//bg2.jpg");
+// 	for(int i =0;i<8;i++)
+// 	{
+// 		char a[100];
+// 		sprintf_s(a,"Images//c%d.png",i+1);
+// 		printf("%s",a);
+// 		charImg[i]=iLoadImage(a); 
+// 	}
+// }
+void initmenubar()
 {
-    bg1 = iLoadImage("Images//bg1.jpg");
-    bg2 = iLoadImage("Images//bg2.jpg");
-	for(int i =0;i<8;i++)
-	{
-		char a[100];
-		sprintf_s(a,"Images//c%d.png",i+1);
-		printf("%s",a);
-		charImg[i]=iLoadImage(a); 
-	}
+    bg = iLoadImage("resources//menu_screen//menu.jpg");
+
 }
 void moveBG()
 {
@@ -72,7 +78,7 @@ int main()
     //iSetTimer(50,moveBG);
 	iInitialize(SCREEN_WIDTH, SCREEN_HEIGHT, "Connected Background Scroll");
 
-    initBackground();
+    initmenubar();
 
     iStart();
     return 0;
