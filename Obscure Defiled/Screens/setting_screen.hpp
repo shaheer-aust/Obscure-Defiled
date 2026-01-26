2b50cf85d0ac70bff357c9d362316ffa1ab440ca
shaheer-aust <shaheer.cse.00724205101034@aust.edu>
2026-01-26 15:18:40 +0600

    f

--- Obscure Defiled/Screens/setting_screen.hpp
+++ Obscure Defiled/Screens/setting_screen.hpp
@@ -14,46 +14,47 @@
 using namespace std;
 struct Setting_screen
 {
-    vector<int> images;;
-    bool lastFrameBackClicked = false;
+	vector<int> images;;
+	bool lastFrameBackClicked = false;
 	long long lastBackBlipTime = 0;
 
-    void initsettingbar()
-    {
-        images.push_back(iLoadImage("resources//menu_screen//menu.jpg"));
-        images.push_back(iLoadImage("resources//menu_screen//Buttons//back.png"));
-        // Load images or resources needed for the settings screen
-    }
-    void drawsetting_screen()
-    {
-        iShowImage(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, images[0]);
-        iShowImage(0, 0, BACK_BUTTON, BACK_BUTTON, images[1]);
-        // Additional drawing code for settings can be added here
-    }
-    //back button hover and click detection for settings
-    bool isBackButtonClicked(int mx, int my)
-    {
-        return (mx >= 0 && mx <= BACK_BUTTON && my >= 0 && my <= BACK_BUTTON);
-    }
+	void initsettingbar()
+	{
+		images.push_back(iLoadImage("resources//menu_screen//menu.jpg"));
+		images.push_back(iLoadImage("resources//menu_screen//Buttons//back.png"));
+		// Load images or resources needed for the settings screen
+	}
+	void drawsetting_screen()
+	{
+		iShowImage(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, images[0]);
+		iShowImage(0, 0, BACK_BUTTON, BACK_BUTTON, images[1]);
+		// Additional drawing code for settings can be added here
+	}
+	//back button hover and click detection for settings
+	bool isBackButtonClicked(int mx, int my)
+	{
+		return (mx >= 0 && mx <= BACK_BUTTON && my >= 0 && my <= BACK_BUTTON);
+	}
 
-    //hover detection
-    void checkButtonHover(int mx, int my)
-    {
-        mciSendString("open \"resources//menu_screen//button_sound//button.mp3\" alias ggsong", NULL, 0, NULL);
-        long long currentTime = glutGet(GLUT_ELAPSED_TIME);
+	//hover detection
+	void checkButtonHover(int mx, int my)
+	{
+		mciSendString("open \"resources//menu_screen//button_sound//button.mp3\" alias ggsong", NULL, 0, NULL);
+		long long currentTime = glutGet(GLUT_ELAPSED_TIME);
 
-        if (isBackButtonClicked(mx, my) && !lastFrameBackClicked)
-        {
-            if (currentTime - lastBackBlipTime > HOVER_COOLDOWN)
-            {
-                mciSendString("play ggsong from 0", NULL, 0, NULL);
-                lastBackBlipTime = currentTime;
-            }
-            lastFrameBackClicked = true;
-        }
-        else if (!isBackButtonClicked(mx, my))
-        {
-            lastFrameBackClicked = false;
-        }
-    };
+		if (isBackButtonClicked(mx, my) && !lastFrameBackClicked)
+		{
+			if (currentTime - lastBackBlipTime > HOVER_COOLDOWN)
+			{
+				mciSendString("play ggsong from 0", NULL, 0, NULL);
+				lastBackBlipTime = currentTime;
+			}
+			lastFrameBackClicked = true;
+		}
+		else if (!isBackButtonClicked(mx, my))
+		{
+			lastFrameBackClicked = false;
+		}
+	};
+};
 #endif
