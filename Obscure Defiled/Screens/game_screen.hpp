e77425fe59cf13031137135f6deb44bf2040ef22
Mohammed Shaheer Imam <imshaheer2004@gmail.com>
2026-02-11 22:12:38 +0600

    Update game_screen.hpp

--- Obscure Defiled/Screens/game_screen.hpp
+++ Obscure Defiled/Screens/game_screen.hpp
@@ -18,10 +18,17 @@ using namespace std;
 struct GameScreen
 {
     vector<int> images;
-    vector<int> character_idle_images;
-    vector<int> character_run_images;
+    vector<int> character_idle_R_images;
+    vector<int> character_idle_L_images;
+    vector<int> character_run_L_images;
+    vector<int> character_run_R_images;
+    int movement_index = 0;
+    bool isMoving = false;
     int x = 0;
+    double characterPosition_X = 100.0;
+    double characterPosition_Y = 100.0;
     double bg_speed = 20.0;
+    double character_speed = 25.0;
     void initgame_screen()
     {
         images.push_back(iLoadImage("resources//game_screen//level_1/bg_1//screen_for_level_1_new.jpg"));
@@ -35,45 +42,91 @@ struct GameScreen
         {
             char a[200];
             sprintf_s(a, "resources//Main_Character//Normal/With Knife//Idle//idle_right_%d.png", i + 1);
-            character_idle_images.push_back(iLoadImage(a));
+            character_idle_R_images.push_back(iLoadImage(a));
+        }
+        for (int i = 1; i <= 4; i++)
+        {
+            char a[200];
+            sprintf_s(a, "resources//Main_Character//Normal/With Knife//Idle//idle_left_%d.png", i + 1);
+            character_idle_L_images.push_back(iLoadImage(a));
+        }
+        // Load character run images
+        for (int i = 1; i <= 4; i++)
+        {
+            char a[200];
+            sprintf_s(a, "resources//Main_Character//Normal/With Knife//Run//walking_left_%d.png", i + 1);
+            character_run_L_images.push_back(iLoadImage(a));
+        }
+        for (int i = 1; i <= 4; i++)
+        {
+            char a[200];
+            sprintf_s(a, "resources//Main_Character//Normal/With Knife//Run//walking_right_%d.png", i + 1);
+            character_run_R_images.push_back(iLoadImage(a));
         }
         // character_idle_images.push_back(iLoadImage("resources//Main_Character//Normal/With Knife//Idle//idle_left_1.png"));
     }
     void show_character_idle()
     {
         int currentIdx = getIdleIndex();
-        iShowImage(100, 100, 64, 64, character_idle_images[currentIdx]);
+        iShowImage(characterPosition_X, characterPosition_Y, 64, 64, character_idle_R_images[currentIdx]);
     }
-
+    void show_character_run()
+    {
+        
+        if (movement_index >= character_run_R_images.size())
+        {
+         
+            movement_index = 0;
+        }
+        iShowImage(characterPosition_X, characterPosition_Y, 64, 64, character_run_R_images[movement_index]);
+        //movement_index++;
+    }
     void handleSpecialKeyboard(unsigned char key)
     {
         // Handle special keyboard input for game controls (e.g., arrow keys for movement)
         if (key == GLUT_KEY_UP)
         {
             // Move player up
+            characterPosition_Y += bg_speed;
+            if (characterPosition_Y > SCREEN_HEIGHT)
+            {
+                characterPosition_Y = SCREEN_HEIGHT;
+            }
         }
         else if (key == GLUT_KEY_DOWN)
         {
             // Move player down
+            characterPosition_Y -= bg_speed;
+            if (characterPosition_Y < 0)
+            {
+                characterPosition_Y = 0;
+            }
         }
         else if (key == GLUT_KEY_LEFT)
         {
             // Move player left
-            x += bg_speed;
-            if (x >= SCREEN_WIDTH)
+            characterPosition_X -= character_speed;
+            if (characterPosition_X < 0)
             {
-                x = 0; // Reset position to create a looping effect
+                characterPosition_X = 0;
             }
         }
         else if (key == GLUT_KEY_RIGHT)
         {
-            // Move player right
-            
-            x -= bg_speed;
-            if (x <= -SCREEN_WIDTH)
+            // Move player right (forward)
+            characterPosition_X += character_speed;
+            if (characterPosition_X > SCREEN_WIDTH)
             {
-                x = 0; // Reset position to create a looping effect
+                characterPosition_X = SCREEN_WIDTH;
             }
+            isMoving = true;
+            movement_index++;
+            
+        }
+        else
+        {
+            isMoving = false;
+            movement_index = 0;
         }
     }
 
@@ -84,7 +137,14 @@ struct GameScreen
         iShowImage(x, 0, SCREEN_WIDTH, SCREEN_HEIGHT, images[0]);
         iShowImage(SCREEN_WIDTH + x, 0, SCREEN_WIDTH, SCREEN_HEIGHT, images[0]);
         // Additional drawing code for settings can be added here
-        show_character_idle();
+        if (isMoving)
+        {
+            show_character_run();
+        }
+        else
+        {
+            show_character_idle();
+        }
     }
 };
 
