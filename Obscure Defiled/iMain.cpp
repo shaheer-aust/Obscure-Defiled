1b52df2039fe9d72c32507b61ee7d59f3972ffde
shaheer-aust <shaheer.cse.00724205101034@aust.edu>
2026-02-12 15:27:44 +0600

    hgfhf

--- Obscure Defiled/iMain.cpp
+++ Obscure Defiled/iMain.cpp
@@ -265,7 +265,7 @@ int main()
 	initIntroScreen();
 	iSetTimer(200, idle_animation);
 	iSetTimer(1000, reset_movement);
-	iSetTimer(50, physics_update);
+	iSetTimer(20, physics_update);
 	iSetTimer(16, character_movement);
 	setting.initsettingbar();
 	// initialize game assets once
