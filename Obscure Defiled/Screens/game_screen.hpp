6028955c46c5505aaaf52c3e6540e28fa3b20e32
Mohammed Shaheer Imam <imshaheer2004@gmail.com>
2026-02-17 01:33:47 +0600

    increase mini enemy size

--- Obscure Defiled/Screens/game_screen.hpp
+++ Obscure Defiled/Screens/game_screen.hpp
@@ -192,7 +192,7 @@ struct GameScreen
         iShowImage(-SCREEN_WIDTH + x, 0, SCREEN_WIDTH, SCREEN_HEIGHT, images[0]);
         iShowImage(x, 0, SCREEN_WIDTH, SCREEN_HEIGHT, images[0]);
         iShowImage(SCREEN_WIDTH + x, 0, SCREEN_WIDTH, SCREEN_HEIGHT, images[0]);
-        // Additional drawing code for settings can be added here
+    
         // cout << "Hero Health: " << hero1.HeroHealth << endl;
         iShowImage(SCREEN_WIDTH / 2 - (275 / 2), SCREEN_HEIGHT - 150, 275, 200, health_bar_images[(hero1.HeroHealth / 10)]);
 
