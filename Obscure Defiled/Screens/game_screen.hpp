47fd147f7e4aacc4466b3b8b31223b0c31a2882f
Mohammed Shaheer Imam <imshaheer2004@gmail.com>
2026-02-17 02:19:41 +0600

    Update game_screen.hpp

--- Obscure Defiled/Screens/game_screen.hpp
+++ Obscure Defiled/Screens/game_screen.hpp
@@ -203,7 +203,7 @@ struct GameScreen
 
             // Calculate index
             int frameIndex = (int)floor((currentHealth / 200.0) * 15);
-            iShowImage(SCREEN_WIDTH - 320, SCREEN_HEIGHT - 150, 300, 57, boss.boss_health_bar_images[frameIndex]);
+            iShowImage(SCREEN_WIDTH - 320, SCREEN_HEIGHT - 150, 200, 50, boss.boss_health_bar_images[frameIndex]);
         }
 
         if (hero1.isAttacking)
