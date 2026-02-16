50bf0e45f872fe942c03ced0b558fd6cbf2108de
Mohammed Shaheer Imam <imshaheer2004@gmail.com>
2026-02-17 00:40:41 +0600

    method separation

--- Obscure Defiled/iMain.cpp
+++ Obscure Defiled/iMain.cpp
@@ -1,5 +1,6 @@
 ﻿
 #include <stdio.h>
+#include <character_functions\hero_functions.cpp>
 #include "iGraphics.h"
 #include "Screens\menu_screen.hpp"
 #include "Screens\setting_screen.hpp"
@@ -288,74 +289,7 @@ void iSpecialKeyboard(unsigned char key)
 /* -------------------- INIT -------------------- */
 
 
-void reset_movement()
-{
-	game.resetMovement();
-}
-void physics_update()
-{
-	game.updatePhysics();
-}
 
-void character_movement()
-{
-	if (game.rightPressed && !game.hero1.isJumping)
-	{
-		game.x -= game.bg_speed;
-		if(game.enemy1.isActive){
-			game.enemy1.enemyPosition_X -= game.bg_speed;
-		}
-		if(game.enemy2.isActive){
-			game.enemy2.enemyPosition_X -= game.bg_speed;
-		}
-		if (game.boss.isActive || game.boss.bossHealth <= 0){
-			game.boss.bossPosition_X -= game.bg_speed;
-		}
-		if (game.x <= -SCREEN_WIDTH)
-		{
-			game.x = 0;
-		}
-		game.hero1.isMoving = true;
-		game.hero1.movement_index++;
-		game.hero1.characterPosition_X += game.hero1.character_speed;
-		if (game.hero1.characterPosition_X >= SCREEN_WIDTH - 70)
-		{
-			game.hero1.characterPosition_X = SCREEN_WIDTH - 70;
-		}
-	}
-	else if (game.leftPressed && !game.hero1.isJumping)
-	{
-		game.x += game.bg_speed;
-		if(game.enemy1.isActive){
-			game.enemy1.enemyPosition_X += game.bg_speed;
-		}
-		if(game.enemy2.isActive){
-			game.enemy2.enemyPosition_X += game.bg_speed;
-		}
-		if(game.boss.isActive || game.boss.bossHealth<=0){
-			game.boss.bossPosition_X += game.bg_speed;
-		}
-		if (game.x >= SCREEN_WIDTH)
-		{  
-			game.x = 0;
-		}
-		game.hero1.isMoving = true;
-		game.hero1.movement_index++;
-		game.hero1.characterPosition_X -= game.hero1.character_speed;
-		if (game.hero1.characterPosition_X < 0)
-		{
-			game.hero1.characterPosition_X = 0;
-		}
-	}
-	else if (game.hero1.isJumping)
-	{
-		game.hero1.isMoving = false;
-	}
-	else
-	{
-		game.hero1.isMoving = false;
-	}
-}
 
 void enemy_movement()
 {
