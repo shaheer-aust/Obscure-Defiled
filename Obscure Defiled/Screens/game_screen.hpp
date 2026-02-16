34de0e851fce6cd4e38a49814d6cb15d612a6385
shaheer-aust <shaheer.cse.00724205101034@aust.edu>
2026-02-17 03:27:02 +0600

    healthbar for boss

--- Obscure Defiled/Screens/game_screen.hpp
+++ Obscure Defiled/Screens/game_screen.hpp
@@ -203,7 +203,7 @@ struct GameScreen
 
             // Calculate index
             int frameIndex = (int)floor((currentHealth / 200.0) * 15);
-            iShowImage(boss.bossPosition_X-60, boss.bossPosition_Y+100 , 122, 50, boss.boss_health_bar_images[frameIndex]);
+            iShowImage(boss.bossPosition_X-10, boss.bossPosition_Y+100 , 122, 20, boss.boss_health_bar_images[frameIndex]);
         }
 
         if (hero1.isAttacking)
