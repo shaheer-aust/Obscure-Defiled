32de144367337533c1b8ff16ac36faa40ca20d9f
Mohammed Shaheer Imam <imshaheer2004@gmail.com>
2026-03-24 14:35:40 +0600

    Update iMain.cpp

--- Obscure Defiled/iMain.cpp
+++ Obscure Defiled/iMain.cpp
@@ -741,6 +741,7 @@ void all_50_ms_ticks()
 				mciSendString("close gamebg3", NULL, 0, NULL);
 
 				mciSendString("play victorysound from 0", NULL, 0, NULL);
+				screens.push("after_level_1");
 				screens.push("victory");
 				counter++;
 			}
@@ -770,6 +771,7 @@ void all_50_ms_ticks()
 				mciSendString("close gamebg2", NULL, 0, NULL);
 
 				mciSendString("play victorysound from 0", NULL, 0, NULL);
+				screens.push("after_level_1");
 				screens.push("victory");
 				counter++;
 			}
